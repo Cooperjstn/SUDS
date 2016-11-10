b1b9ab2055485ae77a73ec543300540b50d206da
Steven Burris <stevenburris.jr@gmail.com>
2016-11-10 17:29:47 -0500

    Added "/user" post-route to add new users and login && added .jar file;

--- src/main/java/com/theironyard/controllers/SudsRestController.java
+++ src/main/java/com/theironyard/controllers/SudsRestController.java
@@ -4,10 +4,19 @@ import com.theironyard.entities.User;
 import com.theironyard.services.BeerRepository;
 import com.theironyard.services.UserRepository;
 import com.theironyard.utilities.PasswordStorage;
+import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 /**
  * Created by stevenburris on 11/10/16.
@@ -20,12 +29,38 @@ public class SudsRestController {
     @Autowired
     UserRepository users;
 
+    Server h2;
+
     @PostConstruct
-    public void init() throws PasswordStorage.CannotPerformOperationException {
-        User defaultUser = new User("Steven", PasswordStorage.createHash("Young"));
+    public void init() throws PasswordStorage.CannotPerformOperationException, SQLException {
+//        start web server
+        h2 = Server.createWebServer().start();
+
+//        add a default user
+        User defaultUser = new User("Admin", PasswordStorage.createHash("hunter2"));
         if (users.findFirstByName(defaultUser.name) == null) {
             users.save(defaultUser);
         }
+    }
+
+    @PreDestroy
+    public void destroy() {
+        h2.stop();
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.POST)
+    public ResponseEntity<User> addUser(HttpSession session, @RequestBody User user) throws PasswordStorage.CannotPerformOperationException, PasswordStorage.InvalidHashException {
+        User userFromH2 = users.findFirstByName(user.getName());
+        if (userFromH2 == null) {
+            user.setPassword(PasswordStorage.createHash(user.getPassword()));
+            users.save(user);
+        }
+        else if(!PasswordStorage.verifyPassword(user.getPassword(), userFromH2.getPassword())){
+            return new ResponseEntity<User>(HttpStatus.FORBIDDEN);
+        }
+
+        session.setAttribute("name", user.getName());
+        return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 
 
