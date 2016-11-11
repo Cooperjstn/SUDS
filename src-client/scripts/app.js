b3a58aef153dedc8a74daf33d1e0f4c12937fbf3
cooperjstn <cooperjstn@gmail.com>
2016-11-11 14:24:58 -0500

    Commit 1 "updated app.js"

--- src-client/scripts/app.js
+++ src-client/scripts/app.js
@@ -2,6 +2,28 @@ const ReactDOM = require('react-dom');
 const React = require('react')
 const Backbone = require('backbone');
 
+
+const AppRouter = Backbone.Router.extend ({
+  routes: {
+    "singleview" : "renderSingleView"
+    "input" : "rendeSUDSInput",
+    "suds"  : "renderMultiSUDSView",
+    "login" : 'renderAuthView'
+  },
+
+renderSingleView: function(){
+  ReactDOM.render(<AppViewController)
+}
+
+
+
+
+
+
+
+})
+
+
 document.querySelector('#app-container').innerHTML = `<h1>Yah okay</h1>`
 
 new AppRouter()
